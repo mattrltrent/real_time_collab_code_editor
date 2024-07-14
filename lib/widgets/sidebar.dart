b4e41e8bf58b24ed4f4002bd5eed2672a8ec3a9d
liamshatzel <liam.shatzel@gmail.com>
2024-07-14 11:37:58 -0700

    weeee

--- lib/widgets/sidebar.dart
+++ lib/widgets/sidebar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Sidebar extends StatefulWidget {
   const Sidebar({super.key});
@@ -9,7 +10,33 @@ class Sidebar extends StatefulWidget {
 
 class _SidebarState extends State<Sidebar> {
   @override
+  final _formKey = GlobalKey<FormState>();
+
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Form(
+        key: _formKey,
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Enter Data',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('Submit'),
+            ),
+          ],
+        ));
   }
 }
