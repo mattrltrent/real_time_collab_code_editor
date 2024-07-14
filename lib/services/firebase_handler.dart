ecf9952d02bebdc47262441779132e715f107453
liamshatzel <liam.shatzel@gmail.com>
2024-07-14 10:48:30 -0700

    fix merge conflicts, services

---
+++ lib/services/firebase_handler.dart
@@ -0,0 +1,3 @@
+import 'package:firebase_database/firebase_database.dart';
+
+DatabaseReference ref = FirebaseDatabase.instance.ref();
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import firebase_database
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
